e1af011dc1633a5d0b2a4ba31b6c405f96d80b12
KoteshwarChinnolla <98650844+KoteshwarChinnolla@users.noreply.github.com>
2025-07-24 15:19:31 +0530

    compose files updated

--- Notifications/src/main/java/com/example/notifications/controller/NotificationController.java
+++ Notifications/src/main/java/com/example/notifications/controller/NotificationController.java
@@ -1,5 +1,5 @@
 package com.example.notifications.controller;
-
+ 
 import com.example.notifications.producer.NotificationProducer;
 import com.example.notifications.service.NotificationPushService;
 import com.example.notifications.entity.Notification;
@@ -9,25 +9,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
+ 
 import java.util.List;
-
+ 
 @RestController
 @RequestMapping("/api/notifications")
-
+ 
 public class NotificationController {
-
+ 
     @Autowired
     private NotificationService notificationService;
-
+ 
     @Autowired
     private NotificationProducer producer;
-
+ 
     @Autowired
     private NotificationPushService pushService;
     @Autowired
     RestTemplate restTemplate;
-
+ 
     @PostMapping("/send")
     public ResponseEntity<String> sendNotification(@RequestBody Notification notification) {
         notificationService.sendNotification(
@@ -58,58 +58,64 @@ public class NotificationController {
         }
         return ResponseEntity.ok("Notification sent");
     }
-
+ 
     @GetMapping("/unread/{user}")
     public ResponseEntity<List<Notification>> getUnread(@PathVariable String user) {
         return ResponseEntity.ok(notificationService.getUnreadNotifications(user));
     }
-
+ 
     @PutMapping("/stared/{messageId}")
     public String Stared(@PathVariable Long messageId){
         notificationService.startMessage(messageId);
         return "done";
     }
-
+ 
+    @PutMapping("/unstared/{messageId}")
+    public String unStared(@PathVariable Long messageId){
+        notificationService.UnStaredMessage(messageId);
+        return "done";
+    }
+ 
     @DeleteMapping("/delete/{messageId}")
     public String delete(@PathVariable Long messageId){
         notificationService.deleteMessage(messageId);
         return "done";
     }
-
+ 
     @GetMapping("/all/{user}")
     public ResponseEntity<List<Notification>> getAll(@PathVariable String user) {
         return ResponseEntity.ok(notificationService.getAllNotifications(user));
     }
-
+ 
     @PostMapping("/read/{id}")
     public ResponseEntity<String> markRead(@PathVariable Long id) {
         notificationService.markAsRead(id);
         return ResponseEntity.ok("Notification marked as read");
     }
-
+ 
     @GetMapping("/unread-count/{receiver}")
     public ResponseEntity<Long> getUnreadCount(@PathVariable String receiver) {
         return ResponseEntity.ok(notificationService.getUnreadCount(receiver));
     }
-
+ 
     @GetMapping("/isOnline/{username}")
     public boolean isOnline(@PathVariable String username){
         return pushService.isOnline(username);
     }
-
+ 
     @GetMapping("/subscribe/{username}")
     public SseEmitter subscribe(@PathVariable String username) {
         return pushService.subscribe(username);
     }
-
+ 
     @GetMapping("/receive/{userId}")
     public SseEmitter receive(@PathVariable String userId) {
         return pushService.subscribe(userId); // Only unread + live messages
     }
-
+ 
     @GetMapping("/unSubscribe/{username}")
     public String unSubscribe(@PathVariable String username){
         return pushService.unSubscribe(username);
     }
-
+ 
 }
--- Notifications/src/main/java/com/example/notifications/service/NotificationService.java
+++ Notifications/src/main/java/com/example/notifications/service/NotificationService.java
@@ -1,14 +1,14 @@
 package com.example.notifications.service;
-
+ 
 import jakarta.transaction.Transactional;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import java.time.LocalDateTime;
-
+ 
 import com.example.notifications.producer.NotificationProducer;
 import com.example.notifications.entity.Notification;
 import com.example.notifications.repository.NotificationRepository;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.core.ParameterizedTypeReference;
@@ -17,47 +17,47 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
+ 
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-
+ 
 @Service
 public class NotificationService {
-
+ 
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
-
-
+ 
+ 
     @Autowired
     private NotificationRepository repository;
-
+ 
     @Autowired
     private NotificationProducer producer;
-
+ 
     @Autowired
     private RestTemplate restTemplate;
-
+ 
     public void sendNotification(String receiver, String message, String sender, String type, String link, String category, String kind,String Subject) {
         if ("team".equalsIgnoreCase(category)) {
             String url = "http://localhost:8090/api/team/employee/" + receiver;
-
+ 
             ResponseEntity<List<Map<String, Object>>> response = restTemplate.exchange(
                     url,
                     HttpMethod.GET,
                     null,
                     new ParameterizedTypeReference<>() {}
             );
-
+ 
             List<Map<String, Object>> teams = response.getBody();
-
+ 
             if (teams != null && !teams.isEmpty()) {
                 Map<String, Object> team = teams.get(0);
                 List<Map<String, Object>> employees = (List<Map<String, Object>>) team.get("employees");
-
+ 
                 for (Map<String, Object> emp : employees) {
                     String employeeId = (String) emp.get("employeeId");
-
+ 
                     Notification notification = Notification.builder()
                             .receiver(employeeId)
                             .message(message)
@@ -71,29 +71,29 @@ public class NotificationService {
                             .subject(Subject)
                             .stared(false)
                             .build();
-
+ 
                     repository.save(notification);
                     producer.sendNotification(notification);
                 }
             }
         } else if ("department".equalsIgnoreCase(category)) {
             String url = "http://localhost:8090/api/department/" + receiver + "/employees";
-
+ 
             ResponseEntity<Map<String, Object>> response = restTemplate.exchange(
                     url,
                     HttpMethod.GET,
                     null,
                     new ParameterizedTypeReference<>() {}
             );
-
+ 
             Map<String, Object> department = response.getBody();
-
+ 
             if (department != null && department.containsKey("employeeList")) {
                 List<Map<String, Object>> employees = (List<Map<String, Object>>) department.get("employeeList");
-
+ 
                 for (Map<String, Object> emp : employees) {
                     String employeeId = (String) emp.get("employeeId");
-
+ 
                     Notification notification = Notification.builder()
                             .receiver(employeeId)
                             .message(message)
@@ -107,13 +107,13 @@ public class NotificationService {
                             .stared(false)
                             .subject(Subject)
                             .build();
-
+ 
                     repository.save(notification);
                     producer.sendNotification(notification);
                 }
             }
         } else {
-
+ 
             Notification notification = Notification.builder()
                     .receiver(receiver)
                     .message(message)
@@ -127,18 +127,18 @@ public class NotificationService {
                     .stared(false)
                     .subject(Subject)
                     .build();
-
+ 
             repository.save(notification);
             producer.sendNotification(notification);
         }
     }
-
-
+ 
+ 
     @Cacheable(value = "unreadNotifications", key = "#receiver")
     public List<Notification> getUnreadNotifications(String receiver) {
         return repository.findByReceiverAndReadFalseOrderByCreatedAtDesc(receiver);
     }
-
+ 
     public void startMessage(Long id){
         repository.findById(id).ifPresent(notification -> {
             notification.setStared(true);
@@ -147,7 +147,16 @@ public class NotificationService {
             evictCache(receiver);
         });
     }
-
+ 
+    public void UnStaredMessage(Long id){
+        repository.findById(id).ifPresent(notification -> {
+            notification.setStared(false);
+            repository.save(notification);
+            String receiver = notification.getReceiver();
+            evictCache(receiver);
+        });
+    }
+ 
     @Transactional
     public boolean deleteMessage(Long id) {
         Optional<Notification> optional = repository.findById(id);
@@ -159,14 +168,13 @@ public class NotificationService {
         }
         return false;
     }
-
-
-
+ 
+ 
     @Cacheable(value = "getAllNotifications", key = "#receiver")
     public List<Notification> getAllNotifications(String receiver) {
         return repository.findByReceiverOrderByCreatedAtDesc(receiver);
     }
-
+ 
     public void markAsRead(Long id) {
         repository.findById(id).ifPresent(notification -> {
             notification.setRead(true);
@@ -175,12 +183,12 @@ public class NotificationService {
             evictCache(receiver);
         });
     }
-
+ 
     @CachePut(value = "unreadCount", key = "#receiver")
     public Long getUnreadCount(String receiver) {
         return repository.countByReceiverAndReadFalse(receiver);
     }
-
+ 
     @CacheEvict(value = {"unreadNotifications", "getAllNotifications", "unreadCount"}, key = "#receiver")
     public void evictCache(String receiver) {
         redisTemplate.delete("unreadNotifications::" + receiver);
@@ -188,5 +196,5 @@ public class NotificationService {
         redisTemplate.delete("unreadCount::" + receiver);
         System.out.println("Deleted Redis cache for receiver: " + receiver);
     }
-
+ 
 }
